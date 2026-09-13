192fa147ad0c21cd9d9ee2407fb5595159658939
Guy Clement Kamdem <gycks.wolf555@slmail.me>
2026-09-13 21:28:15 +0200

    make new translators discoverable

--- babelfishers/core/translators/__init__.py
+++ babelfishers/core/translators/__init__.py
@@ -1,1 +1,4 @@
+from .azure_translator import AzureTranslator  # noqa: F401
 from .deepl_translator import DeeplTranslator  # noqa: F401
+from .google_translator import GoogleTranslator  # noqa: F401
+from .libretrans_translator import LibreTranslateTranslator  # noqa: F401
