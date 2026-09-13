221636c65e14fa88ba6ae9569990fe2324a9ed30
Guy Clement Kamdem <gycks.wolf555@slmail.me>
2026-09-13 15:07:07 +0200

    make new parsers discoverable

--- babelfishers/core/parsers/__init__.py
+++ babelfishers/core/parsers/__init__.py
@@ -1,1 +1,12 @@
-from . import json_parser  # noqa: F401
+from . import (  # noqa: F401
+    android_strings_parser,
+    apple_strings_parser,
+    dotnet_resx_parser,
+    flutter_arb_parser,
+    gettext_parser,
+    html_parser,
+    java_properties_parser,
+    json_parser,
+    xliff_parser,
+    yaml_parser,
+)
