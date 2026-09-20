ae3d48cc36d411ad8d94ae488ed5e3e1fb5fcbc7
Guy Clement Kamdem <gycks.wolf555@slmail.me>
2026-09-20 13:56:03 +0200

    get_env optionally raise error if env not found

--- babelfishers/utils/utils.py
+++ babelfishers/utils/utils.py
@@ -7,12 +7,14 @@ from pathlib import Path
 from babelfishers import APPLICATION_NAME
 
 
-def get_env(name: str) -> str:
+def get_env(name: str, raise_on_error: bool = True) -> str:
     if name == "" or name == " " or name is None:
         raise KeyError(f"Environment variable {name} is not set")
 
     value = os.getenv(name)
     if value is None:
+        if not raise_on_error:
+            return ""
         raise KeyError(f"Environment variable {name} is not set")
 
     return value
